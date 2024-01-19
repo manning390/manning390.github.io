53635f237b541d02b02839013baf5e9517a35089
Michael Manning <michael@manning390.com>
2024-01-19 00:09:34 -0800

    Lol svelte has docs on this. Wow

--- svelte.config.js
+++ svelte.config.js
@@ -8,9 +8,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: '404.html'
+		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/manning390.github.io' : '',
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		},
 	},
 };
